a97a3c99187dcdbc045d1534d3c3e2ed0e808875
ujjwaliitr <80790486+ujjwaliitr@users.noreply.github.com>
2024-07-18 22:40:16 +0530

    working button 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,10 @@ dependencies {
     implementation(libs.horologist.compose.tools)
     implementation(libs.horologist.tiles)
     implementation(libs.watchface.complications.data.source.ktx)
+    implementation("com.google.android.material:material:1.4.0")
     androidTestImplementation(platform(libs.compose.bom))
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
 }
